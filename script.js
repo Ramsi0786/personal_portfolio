346a5889b256cb681241390e7d141080a6eab4e4
Ramsi0786 <ramsisidheek7000@gmail.com>
2025-05-07 16:16:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(window).on("scroll", function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop >= 70) {
+            $('body').addClass('fixed-header');
+        } else {
+            $('body').removeClass('fixed-header');
+        }
+    });
+});
